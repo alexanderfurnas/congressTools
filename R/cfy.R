d702057084e2a37a96ad5987a6efabb819b40130
Alexander Furnas <alexanderfurnas@MacBook-Pro-3.local>
2019-02-16 19:12:47 -0500

    Added v 0.900 of the congressTools package

---
+++ R/cfy.R
@@ -0,0 +1,14 @@
+#' Year from Congress
+#'
+#' This function takes a year and returns the numerical value of the session of Congress during that year.
+#' 
+#' 
+#' @param year the numerical value of the year for which you would like to know
+#' @return a numeric value or vector of values of the Congress or Congresses which correspond the years
+#' @export
+cfy <- function(year){
+  c = (year - 1789)/2 + 1 
+  c <- c %/% 1
+  return(c)
+}
+
---
+++ R/yfc.R
@@ -0,0 +1,21 @@
+#' Year from Congress
+#'
+#' This function takes the number of a Congress and returns the years that Congress was in session. 
+#' It will return either the first or second year of the session of Congress.
+#' 
+#' 
+#' @param congress the numerical signifier for congress. Either a single value of a vector of values.
+#' @param year Which year of the Congress to return. Rakes either 'first' or 'second', defaults to "first" 
+#' @return a numeric value or vector of values of the years which correspond the Congress
+#' @export
+
+yfc <- function(congress, year = "first"){
+  if (year == "first"){
+    y <- 2*(congress-1) + 1789 
+  }  else if (year == "second") {
+    y <- 2*(congress-1) + 1790 
+  }
+  return(y)
+}
+
+
